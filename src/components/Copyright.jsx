9d2a091b9872c1c76d4cc56c5b955803c457ad05
gjrmd953 <gjrmd953@gmail.com>
2026-02-04 23:32:52 +0600

    edit

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Container from './Container'
 
 const Copyright = () => {
   return (
     <div className='py-13 bg-emerald-50'>
-        <div className="">
-            <h2>
+        <Container>
+            <div className="text-center">
+            <h2 className=''>
                 Copyright GJR 2026
             </h2>
         </div>
+        </Container>
     </div>
   )
 }
