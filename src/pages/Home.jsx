409ab626763214e22e3ea98e0b4cc4f2530827d0
gjrmd953 <gjrmd953@gmail.com>
2026-02-04 23:25:38 +0600

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Copyright from '../components/Copyright'
 
 const Home = () => {
   return (
    <>
    <Navbar/>
+   <Copyright/>
    </>
   )
 }
