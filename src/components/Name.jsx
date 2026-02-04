38843e632e4c22afd17678b3240284a78f3a3cfd
gjrmd953 <gjrmd953@gmail.com>
2026-02-05 00:56:11 +0600

    edit

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -5,16 +5,20 @@ const Name = () => {
   return (
     <div className='py-10'>
             <Container>
-                <div className="flex">
-                    <div className="w-1/6">hi</div>
-                    <div className="w-4/6">
-                     <div className="bg-amber-500">
-                        <h1>
+                <div className="flex items-center">
+                    <div className="w-1/7">logo</div>
+                    <div className="w-5/7">
+                     <div className="text-center">
+                        <h1 className='font-POPIN font-bold text-[40px] uppercase text-red-400'>
                         At-Tahfiz Wassunnah Madrasah Dhaka
                      </h1>
                      </div>
                     </div>
-                    <div className="w-1/6">hi</div>
+                    <div className="w-1/7">
+                    <div className="text-center">
+                        <h2>logo</h2>
+                    </div>
+                    </div>
                 </div>
             </Container>
     </div>
