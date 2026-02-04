b8967a0c48f28aaf51f305c1c8fc4a4b08ede772
gjrmd953 <gjrmd953@gmail.com>
2026-02-04 21:44:55 +0600

    edit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-amber-700'>
+        <h2>
+            hello
+        </h2>
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
-    <div>
-        <h2>hello</h2>
-    </div>
+   <>
+   <Navbar/>
+   </>
   )
 }
 
