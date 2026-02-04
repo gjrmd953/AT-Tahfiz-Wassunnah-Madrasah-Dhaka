64237c9b1ce9370bd216b60340b55762228aa717
gjrmd953 <gjrmd953@gmail.com>
2026-02-04 21:33:11 +0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Home/>
     </>
   )
 }
