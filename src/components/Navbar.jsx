f72275f12e175eff52a196fc6749b321159a501c
gjrmd953 <gjrmd953@gmail.com>
2026-02-05 01:10:26 +0600

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Container from './Container'
+import MainImg from "../assets/img.png"
 
 const Navbar = () => {
   return (
-    <div className='py-11 bg-[#008641]'>
+    <div className='py-11 bg-[#008641] h-screen bg-cover bg-center' style={{ backgroundImage: `url(${MainImg})` }}>
       <Container>
         <div className="items-center">
           <ul className='flex justify-between items-center'>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>Home</li>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>About</li>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>Faculty</li>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>Admission</li>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>Gallery</li>
-            <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer'>Notice</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Home</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>About</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Faculty</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Admission</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Gallery</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Notice</li>
             <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer py-2 px-5 bg-amber-400'>contact us</li>
           </ul>
         </div>
