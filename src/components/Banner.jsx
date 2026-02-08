2bad2131411382a3e533d189a4aafaca8c018ce3
gjrmd953 <gjrmd953@gmail.com>
2026-02-08 23:34:09 +0600

    edit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import Container from './Container'
 
 const Banner = () => {
   return (
-    <div>
+    <div className='py-13 bg-amber-400'>
         <Container>
             <h2>banner</h2>
         </Container>
