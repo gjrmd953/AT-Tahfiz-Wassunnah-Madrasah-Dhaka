8a7fb45792fc01bc0089ca2f381b7aed8f6f7530
gjrmd953 <gjrmd953@gmail.com>
2026-02-10 22:55:21 +0600

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
             <div>
               <h3 className="text-lg font-semibold text-white mb-4">Services</h3>
               <ul className="space-y-2">
-                <li>Web Development</li>
+                <li>Admission</li>
                 <li>UI/UX Design</li>
                 <li>Frontend Projects</li>
                 <li>Consultancy</li>
