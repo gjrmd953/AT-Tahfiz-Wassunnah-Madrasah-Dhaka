05ea8263da2e509b58267ec67bdfba2cdb103e95
gjrmd953 <gjrmd953@gmail.com>
2026-02-10 22:53:28 +0600

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Home</li>
             <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>About</li>
             <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Faculty</li>
-            <li className='font-POPIN font-bold text-[21px] text-[#000000] cursor-pointer'>Admission</li>
+            <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Admission</li>
             <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Gallery</li>
             <li className='font-POPIN font-bold text-[22px] text-[#000000] cursor-pointer'>Notice</li>
             <li className='font-POPIN font-bold text-[22px] text-[#ffff] cursor-pointer py-2 px-5 bg-amber-400'>contact us</li>
